99c1b7f1e27d43145823985e113a0d093d40f71a
Eric Barbosa <ericcarmobarbosa@gmail.com>
2018-10-21 08:32:06 -0300

    Adicionar arquivos de projeto.

---
+++ SportizeMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportizeMvc.Startup))]
+namespace SportizeMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
